1434c8f5d525777d50ed3de492666ccd8b2c5bb7
MaIsabelSolano <misolanob@gmail.com>
2021-05-27 10:16:53 -0600

    cambios

--- src/Grafo.java
+++ src/Grafo.java
@@ -1,4 +1,60 @@
-public class Grafo<T> {
+//referencia: https://stackabuse.com/graphs-in-java-representing-graphs-in-code/
 
-    private class Centro<T>{}
+import java.util.List;
+import java.util.Map;
+
+public class Grafo<Centro> {
+
+    private Map<Centro, List<Centro>> adjCentros;
+    private int numOfNodes;
+
+    private float[][] matrix;
+    private boolean[][] isSetMatrix;
+
+    private class Centro{
+        final String nombre;
+
+        private Centro(String nombre) {
+            this.nombre = nombre;
+        }
+    }
+
+    public Grafo(int numOfNodes) {
+        this.numOfNodes = numOfNodes;
+
+        matrix = new float[numOfNodes][numOfNodes]; //matriz cuadrada
+        isSetMatrix = new boolean[numOfNodes][numOfNodes];
+    }
+
+    public void AgregarCentro(int src, int destino, float peso){
+        float valorPeso = peso;
+
+        matrix[src][destino] = peso;
+        //como siempre va a ser direccionado no es nec
+    }
+
+    public void printMatrix(){
+        for (int i = 0; i <numOfNodes; i++){
+            for (int j = 0; j < numOfNodes; j ++){
+                if (isSetMatrix[i][j]){
+                    System.out.format("%8s",String.valueOf(matrix[i][j]));
+                }
+                else {
+                    System.out.format("%8s","/");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public boolean hasEdge(int src, int destination){
+        return isSetMatrix[src][destination];
+    }
+
+    public Float getEdgeValue(int src, int destination){
+        if (isSetMatrix[src][destination]){
+            return null;
+        }
+        return matrix[src][destination];
+    }
 }
--- src/Lector.java
+++ src/Lector.java
@@ -1,2 +1,23 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Lector {
+
+    Scanner scanDoc;
+    ArrayList<String> nombresCentros = new ArrayList<>();
+
+    Lector () throws FileNotFoundException{
+        scanDoc = new Scanner("guategrafo.txt");
+    }
+
+    public int NumOfcentros() throws FileNotFoundException {
+        int noNodos = 0;
+        //if (scanDoc.nextLine()){}
+        return noNodos;
+    }
+
+    public Grafo GenerarGrafo(Grafo grafo) throws FileNotFoundException{
+        return grafo;
+    }
 }
